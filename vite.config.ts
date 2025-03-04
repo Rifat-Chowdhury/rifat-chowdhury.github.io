09a199d95d88127309e6494a7b11900248014a0a
Rifat-Chowdhury <rifatchowdhury619@gmail.com>
2025-03-04 00:13:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Rifat-Chowdhury.github.io/',  // Use your repo name here
+  base: "/",  // Use your repo name here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
